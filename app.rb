9a8c0f421d7f4eebadb1cf6eec10ec9aae4787ba
Kenny Clare <kmitchellclare@gmail.com>
2022-06-05 15:19:32 -0700

    commit tree cb15eca52376191b187f4049af7c1a0db6810303

---
+++ app.rb
@@ -0,0 +1,14 @@
+# app.rb
+
+# The p method will output the data in a nicer format by calling the .inspect method on our data.
+
+# Ruby has one more method, pp, for "pretty-printing" complex data, such as nested arrays and hashes (the Ruby equivalent of a JavaScript object)
+
+# nil means no value, like null in JS
+
+# puts method always returns nil
+
+# In Ruby, it's convention to use underscores (_) to separate words in variables. This is referred to as snake case (as opposed to camel case, which is the convention in JavaScript).
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
